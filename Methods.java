6118db5c9d5b09404e47e51149224bc98d847acb
Derikprince430 <derikprincebubby1@gmail.com>
2024-10-01 16:19:26 +0530

    1st time code

---
+++ Methods.java
@@ -0,0 +1,21 @@
+public class Methods{
+	 
+	 public static void main(String[] args){
+		 sub(16,5);
+		 add(16,5);
+	 }
+		 public static void sub(int num1, int num2){
+			System.out.println(num1-num2);
+	     }
+		 public static void add(int num1, int num2){
+			 
+			 System.out.println(num1 + num2);
+			 
+		 
+
+        }
+	 
+	 
+	 
+	 
+     }
